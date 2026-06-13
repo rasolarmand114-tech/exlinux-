8884704f4a6e3119e27d6c11e679fa6f98050095
PM99 <rasolarmand114@gmail.com>
2026-06-13 03:46:51 +0330

    Update build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-
 export PLATFORM_VERSION=13
 export ANDROID_MAJOR_VERSION=t
 export ARCH=arm64
 
 make ARCH=arm64 exlinux_defconfig
-make ARCH=arm64 -j$(nproc)
+
+# ساخت کرنل به همراه ماژول‌ها
+make ARCH=arm64 modules -j$(nproc)
